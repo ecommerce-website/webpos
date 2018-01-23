1a230308a921e48b9cc5b99e487a871776e2ec9c
WebS2B <ducvuk6196@gmail.com>
2018-01-23 20:06:25 +0700

    fix js

--- js/controllers/WebposController.js
+++ js/controllers/WebposController.js
@@ -6,15 +6,16 @@ angular.module('WebposApp').controller('WebposController', function($rootScope, 
 
         products.forEach(function(product) {
             Product.addProduct(product);
-            Product.setProductSelect(product);
+            // Product.setProductSelect(product);
         });
 
-        Product.setValueProductSelect("name", "Regularrrrr");
+        // Product.setValueProductSelect("name", "Regularrrrr");
 
         $log.info(Product);
-        $log.info(CallApi.callApiGet("https://api.themoviedb.org/3/movie/popular?api_key=931e54c126cfca6e31b0c905d36d6614&language=en-US&page=1"));
+        // $log.info(CallApi.callApiGet("https://api.themoviedb.org/3/movie/popular?api_key=931e54c126cfca6e31b0c905d36d6614&language=en-US&page=1"));
     });
 
+    // $scope.listinvoice = Invoice.
     products = [{
             id: "001",
             type: "Regular Product",
