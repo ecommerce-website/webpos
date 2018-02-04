6dd18bd5cfca1a208d8ecff73118bca31189ba96
duydinh <duydinh77.ptit@gmail.com>
2018-02-04 08:59:41 +0700

    Inventory Controller

--- js/controllers/OfficeController.js
+++ js/controllers/OfficeController.js
@@ -18,39 +18,13 @@ angular.module('WebposApp').controller('OfficeController', function($rootScope, 
 
     });
 
-    invoices = [{
-            invoice_id : "001",
-            invoice_customer_id : "C001",
-            invoice_total : "1000",
-            invoice_quantity_bought : "5",
-            invoice_transaction : "Normal",
-            invoice_ref : "1-1515767727387",
-            invoice_remark : "note 1",
-            invoice_payment_term : "dasd",
-            invoice_status: "posted",
-            invoice_date: "7-7-1997",
-        },
-        {
-            invoice_id : "003",
-            invoice_customer_id : "C0321",
-            invoice_total : "10320",
-            invoice_quantity_bought : "10",
-            invoice_transaction : "Normal",
-            invoice_ref : "ABD5767727387",
-            invoice_remark : "note 2",
-            invoice_payment_term : "dasxsd",
-            invoice_status: "posted",
-            invoice_date: "10-10-1010",
-        },
-    ];
 
-        CallApi.callRestApiGet('products').then(function(data){
-            $scope.invoices = data.data.data;
-            console.log(data.data.data);
-            data.data.data.forEach(function(invoice) {
-                Invoice.addInvoice(invoice);
-                Invoice.setInvoiceSelect(invoice);
-            });
+    CallApi.callRestApiGet('products').then(function(data){
+        $scope.invoices = data.data.data;
+        console.log(data.data.data);
+        data.data.data.forEach(function(invoice) {
+            Invoice.addInvoice(invoice);
+            Invoice.setInvoiceSelect(invoice);
         });
 
         $scope.invoices = Invoice.listInvoice;
