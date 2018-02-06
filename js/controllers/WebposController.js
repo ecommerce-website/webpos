9ab9cb3b0d3c457f4553d8073a7fe6d0d2796044
Nga <hoangngocnga1997ns@gmail.com>
2018-02-06 19:36:44 +0700

    test

--- js/controllers/WebposController.js
+++ js/controllers/WebposController.js
@@ -20,7 +20,9 @@ angular.module('WebposApp').controller('WebposController', function($rootScope, 
     $scope.tmp = 0;
     $scope.subTotal = 0;
     $scope.total = 0;
-    $scope.tax = 0.15;
+    $scope.tax = 0;
+    $scope.amount = 0;
+    $scope.change = 0;
 
     $scope.addProductToCart = function(product) {
 
@@ -63,6 +65,10 @@ angular.module('WebposApp').controller('WebposController', function($rootScope, 
         }
     };
 
+    $scope.emptyCart = function(){
+        $scope.carts = [];
+    };
+
     $scope.subTotal = function() {
         var subTotal = 0;
         angular.forEach($scope.carts, function(product) {
@@ -77,4 +83,55 @@ angular.module('WebposApp').controller('WebposController', function($rootScope, 
 
         return subTotal - subTotal * $scope.tax;
     }
+
+    $('.checkout').click(function(event) {
+        // $("#main_trans_1").addClass('bounceOutRight');
+        $('#main_trans_1').css('display', 'none');
+        $('#main_trans_2').css('display', 'block');
+    });
+    $('#payment_back_href').click(function(event) {
+        $('#main_trans_1').css('display', 'block');
+        $('#main_trans_2').css('display', 'none');
+    });
+
+
+
+
+
+    $(document).ready(function() {
+    // $('#salesman_det_div').toggle(function () {
+    //  
+    // });
+        $('#salesman_det_div').click(function(event) {
+            $("#transaction_actions_container_normal").css('display', 'none');
+            $('#transaction_actions_container_salesman').css('display', 'block');
+        });
+        $('#salesman_det_div_select').click(function(event) {
+            $('#transaction_actions_container_salesman').css('display', 'none');
+            $('#transaction_actions_container_normal').css('display', 'block');
+        });
+
+        $('#check').click(function() {
+            $('#other_payment_details').load('views/check.html');
+        });
+
+        $('#gift').click(function() {
+            $('#other_payment_details').load('views/gift.html');
+        });
+
+        $('#other').click(function() {
+            $('#other_payment_details').load('views/other.html');
+        });
+
+        $('#print').click(function () {
+            var print = document.getElementById('in');
+            var wme = window.open("","","width=900, height = 700");
+            wme.document.write(print.outerHTML);
+            wme.document.close();
+            wme.focus();
+            wme.print();
+            wme.close();
+        });
+
+    });
 });
